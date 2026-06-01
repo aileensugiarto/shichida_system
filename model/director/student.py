5fb5acda84c556350c7aa6007d2c2d809e42a34f
aileensugiarto <aileensugiarto07@gmail.com>
2026-06-01 20:53:08 +0700

    update student page director portal

--- model/director/student.py
+++ model/director/student.py
@@ -73,7 +73,11 @@ def model_director_student():
           ON sp.id_teacher = t.id_teacher
 
         WHERE a.id_director = %s
-        AND (sp.status IS NULL OR sp.status != 'Trial')
+        AND sp.status IN (
+            'Current Student',
+            'Waiting List',
+            'Past Student',
+        )
         ORDER BY b.branch_name, s.name
     """, (filter_year, filter_month, session['id_director']))
 
